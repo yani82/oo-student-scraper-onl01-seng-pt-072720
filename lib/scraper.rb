6241b6def45ae2b25ea4d30bacea97469783b750
Yani Asari <yani.iman1@gmail.com>
2020-09-07 02:03:46 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,15 +1,43 @@
+require 'nokogiri'
 require 'open-uri'
 require 'pry'
 
 class Scraper
 
   def self.scrape_index_page(index_url)
+    doc = Nokogir::HTML(open(index_url))
+    doc.css('.student-card').map do |student| 
+      {
+        :name => student.css('.student-name').text,
+        :location => student.css('.student-location').text,
+        :profile_url => student.css('a').first['href']
+      }
+    end 
     
   end
 
   def self.scrape_profile_page(profile_url)
-    
+    profile - Nokogiri::HTML(open(profile_url))
+    profile_hash = profile.css('.vitals-container').map do |detail| 
+      {
+        binding.pry 
+        :bio => de 
+        :profile_q =>
+        :twitter => 
+        :linkedin => 
+        :github => 
+        :blog => 
+      }
+    end 
+    profile_hash[0]
   end
 
 end
+
+#student_card = doc.css('.student-card')
+#student_names = student_card.css('.student-name').text 
+#student_locations = student_card.css('.student-location').text 
+#student_profile = student_card.css('a').first[href]
+
+#details-container 
 
